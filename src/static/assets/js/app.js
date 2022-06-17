c6a6ce202d6cbdda5136b86eee95aefb2073c3d0
T. J. Silva <thiago.silva@imd.ufrn.br>
2022-06-17 18:26:03 -0300

    refactor: update development scripts

--- src/static/assets/js/app.js
+++ src/static/assets/js/app.js
@@ -1,39 +1,41 @@
 const getPokemonURL = id => `https://pokeapi.co/api/v2/pokemon/${ id }`
-const generatePokemonPromises = () => Array(150).fill().map((_, index) => fetch(getPokemonURL(index + 1)).then(response => response.json()))
+
+const generatePokemonPromises = () => Array(150)
+  .fill()
+  .map((_, index) => fetch(getPokemonURL(index + 1))
+    .then(response => response.json()))
+
 const pokemonPromises = generatePokemonPromises()
 
-const fetchPokemon = () => {
+const generateHTML = pokemons => pokemons.reduce((accumulator, { id, name, sprites, types }) => {
+  const pokemonTypes = types.map(typeInfo => typeInfo.type.name)
+  const pokemonSprites = Object.create(sprites.other.dream_world)
 
-  Promise.all(pokemonPromises).then(pokemons => {
-    return pokemons.reduce((accumulator, pokemon) => {
-      const types = pokemon.types.map(typeInfo => typeInfo.type.name)
-      const sprites = Object.create(pokemon.sprites.other.dream_world)
+  accumulator += `
+    <li class='card'>
+      <div class='card-header'>
+        <span class='pokemon-id'>#${ id }</span>
+        <h2 class='pokemon-name'>${ name }</h2>
+      </div>
+      <div class='card-content pokemon ${ pokemonTypes[0] }'>
+        <div class='card-body'>
+          <img class='pokemon-img' src='${ pokemonSprites.front_default }' alt='${ name }' width='192px' height='192px'>
+        </div>
+      </div>
+      <div class='card-footer'>
+        <p class='pokemon-types'>${ pokemonTypes.join(' | ') }</p>
+      </div>
+    </li>
+  `
 
-      accumulator += `
-        <li class='card'>
-          <div class='card-header'>
-            <span class='pokemon-id'>#${ pokemon.id }</span>
-            <h2 class='pokemon-name'>${ pokemon.name }</h2>
-          </div>
-          <div class='card-content pokemon ${ types[0] }'>
-            <div class='card-body'>
-              <img class='pokemon-img' src='${ sprites.front_default }' alt='${ pokemon.name }' width='192px' height='192px'>
-            </div>
-          </div>
-          <div class='card-footer'>
-            <p class='pokemon-types'>${ types.join(' | ') }</p>
-          </div>
-        </li>
-      `
+  return accumulator
+}, '')
 
-      return accumulator
-    }, '')
-
-  })
-    .then(pokemons => {
-      const list = document.querySelector('[data-js=\'pokedex\']')
-      list.innerHTML = pokemons
-    })
+const insertPokemonsIntoPage = pokemons => {
+  const list = document.querySelector('[data-js=\'pokedex\']')
+  list.innerHTML = pokemons
 }
 
-fetchPokemon()
+Promise.all(pokemonPromises)
+  .then(generateHTML)
+  .then(insertPokemonsIntoPage)
